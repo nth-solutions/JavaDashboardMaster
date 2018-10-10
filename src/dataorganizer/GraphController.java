9ba57d7d01ed7f3794510717a2ad039c8a9a5e2d
Vineeth Voruganti <18vvoruganti.stem@gmail.com>
2018-10-10 19:17:01 -0400

    Commented first 200 lines of the GraphController Class

--- src/dataorganizer/GraphController.java
+++ src/dataorganizer/GraphController.java
@@ -1,15 +1,6 @@
 package dataorganizer;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.Serializable;
-import java.lang.reflect.InvocationTargetException;
 import java.net.URL;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
@@ -17,10 +8,8 @@ import java.util.Collections;
 import java.util.List;
 import java.util.ResourceBundle;
 import java.util.Set;
-import java.util.concurrent.atomic.AtomicReference;
 
 import javax.swing.JFileChooser;
-import javax.swing.SwingUtilities;
 
 import javafx.application.Platform;
 import javafx.beans.property.ObjectProperty;
@@ -33,23 +22,18 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
 import javafx.geometry.Point2D;
-import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.*;
-import javafx.scene.input.DataFormat;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.FlowPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.beans.binding.BooleanBinding;
 import javafx.scene.text.Text;
-import javafx.stage.Stage;
 
 /*** The Following Refers to Features That Need To Be Implemented Within the Program***/
 //TODO: Add Ability to Graph Mag Data (Possibly Fixed?)
@@ -126,7 +110,7 @@ public class GraphController implements Initializable{
 	private double xRangeLow;
 	private double xRangeHigh;
 	private Rectangle currentTimeInMediaPlayer;																			//Frame-By-Frame Analysis Bar
-	private final Rectangle zoomRect = new Rectangle();
+	private final Rectangle userCreatedZoomRectangleBox = new Rectangle();
 	private final Rectangle baselineRect = new Rectangle();
 	private int XOffsetCounter = 0;
 	private int XOffsetCounterTwo = 0;
@@ -135,100 +119,100 @@ public class GraphController implements Initializable{
 	private int numDataSets;
 	private DecimalFormat roundTime = new DecimalFormat("#.#");
 
-
+	
 	/*** Event Handlers ***/
 
 	@FXML
-	public void handleZoom(ActionEvent event) {
-		setUpZooming(zoomRect, lineChart);
-		doZoom(zoomRect, lineChart);
+	public void handleZoom(ActionEvent event) {																			//TODO
+		setUpZooming(userCreatedZoomRectangleBox, lineChart);
+		doZoom(userCreatedZoomRectangleBox, lineChart);
 	}
 	
 	@FXML 
-	public void handleBaselineRange(ActionEvent event) {
+	public void handleBaselineRange(ActionEvent event) {																//TODO
 		setUpBaselineRangeSelection(baselineRect, lineChart);
 	}
 
 	@FXML
-	public void handleReset(ActionEvent event) {
-		xAxis.setUpperBound(Double.parseDouble(roundTime.format(dataCollector[0].getLengthOfTest())));
-		xAxis.setLowerBound(0);
-		yAxis.setUpperBound(yMax);
-		yAxis.setLowerBound(yMin);
+	public void handleReset(ActionEvent event) {																		//Resets the Graph to its default parameters (y-Axis scale, x-Axis scale and userCreatedZoomRectangleBox is reset to (0,0))
+		xAxis.setUpperBound(Double.parseDouble(roundTime.format(dataCollector[0].getLengthOfTest())));					//Sets the Graph's x-Axis maximum value to the total time of the test
+		xAxis.setLowerBound(0);																							//Sets the Graph's x-Axis minimum value to 0 - the location of the very first data sample
+		yAxis.setUpperBound(yMax);																						//Sets the Graph's y-Axis maximum value to the defined y-Axis maximum (5 by default - varies based on user entry)
+		yAxis.setLowerBound(yMin);																						//Sets the Graph's y-Axis minimum value to the defined y-Axis minimum (-5 by default - varies based on user entry)
 
-		zoomRect.setWidth(0);
-		zoomRect.setHeight(0);
+		userCreatedZoomRectangleBox.setWidth(0);																		//Sets the Width of user's drag and drop zoom rectangle back to its original width value (0)
+		userCreatedZoomRectangleBox.setHeight(0);																		//Sets the Height of the user's drag and drop zoom rectangle back to its original height value (0)
 		
-		if(dataSeries != null) {
-			for (final DataSeries ds : dataSeries) {
-				ds.updateZoom(xAxis.getLowerBound(), xAxis.getUpperBound());
+		if(dataSeries != null) {																						//If the first data series exists (contains data) ->
+			for (final DataSeries axisOfDataSeries : dataSeries) {														//Iterates through each axis of the first data series
+				axisOfDataSeries.updateZoom(xAxis.getLowerBound(), xAxis.getUpperBound());								//Updates the boundaries of the graph for each axis of the first data series
 			}	
 		}
 		
-		if(dataSeriesTwo != null) {
-			for (final DataSeries ds : dataSeriesTwo) {
-				ds.updateZoom(xAxis.getLowerBound(), xAxis.getUpperBound());
+		if(dataSeriesTwo != null) {																						//If the second data series exists (contains data) ->
+			for (final DataSeries axisOfDataSeriesTwo : dataSeriesTwo) {												//Iterates through each axis of the second data series
+				axisOfDataSeriesTwo.updateZoom(xAxis.getLowerBound(), xAxis.getUpperBound());							//Updates the boundaries of the graph for each axis of the second data series
 			}	
 		}
 		
-		repopulateData();
-		restyleSeries();
+		repopulateData();																								//TODO
+		restyleSeries();																								//TODO
 
 	}
 
 	@FXML
-	public void handleDisplayRawData(ActionEvent event) {
-		displaySignedDataCheckbox.setSelected(false);
-		for (DataSeries ds: dataSeries) {
-			ds.setDataConversionType(0);
-			ds.updateZoom(xAxis.getLowerBound(), xAxis.getUpperBound());
-			lineChart.getData().clear();
-			populateData(dataSeries, lineChart);
-			styleSeries(dataSeries, lineChart);
+	public void handleDisplayRawData(ActionEvent event) {																//Event handler that updates the data series to be raw data, then repopulates it within the lineChart
+		displaySignedDataCheckbox.setSelected(false);																	//Deselects the displaySignedDataCheckbox
+		lineChart.getData().clear();																				    //Removes all series currently displayed on the graph to prevent multiple data series from populating the lineChart
+		for (DataSeries axisOfDataSeries: dataSeries) {																	//Iterates through each axis of the first data series
+			axisOfDataSeries.setDataConversionType(0);																	//Sets the data conversion type to 0 (0 is the numeral used to indicate a conversion to raw data) and applies the conversion to each axis of the first data series
+			axisOfDataSeries.updateZoom(xAxis.getLowerBound(), xAxis.getUpperBound());									//Updates the boundaries of the graph for each axis of the first data series
 		}
+		populateData(dataSeries, lineChart);																			//Repopulates the lineChart with the updated data within the dataSeries object
+		styleSeries(dataSeries, lineChart);																				//TODO
 	}
 
 	@FXML
-	public void handleDisplaySignedData(ActionEvent event) {
-		displayRawDataCheckbox.setSelected(false);
-		lineChart.getData().clear();
-		for (DataSeries ds: dataSeries) {
-			ds.setDataConversionType(1);
-			ds.updateZoom(xAxis.getLowerBound(), xAxis.getUpperBound());
+	public void handleDisplaySignedData(ActionEvent event) {															//Event handler that updates the data series to be signed data, then repopulates it within the lineChart
+		displayRawDataCheckbox.setSelected(false);																		//Deselects the displayRawDataCheckbox
+		lineChart.getData().clear();																					//Removes all series currently displayed on the graph to prevent multiple data series from populating the lineChart
+		for (DataSeries axisOfDataSeries: dataSeries) {																	//Iterates through each axis of the first data series
+			axisOfDataSeries.setDataConversionType(1);																	//Sets the data conversion type to 1 (1 is the numeral used to indicate a conversion to signed data) and applies the conversion to each axis of the first data series
+			axisOfDataSeries.updateZoom(xAxis.getLowerBound(), xAxis.getUpperBound());									//Updates the boundaries of the graph for each axis of the first data series
 		}
-		populateData(dataSeries, lineChart);
-		styleSeries(dataSeries, lineChart);
+		populateData(dataSeries, lineChart);																			//Repopulates the lineChart with the updated data within the dataSeries object
+		styleSeries(dataSeries, lineChart);																				//TODO
 
 	}
 
 	@FXML
-	public void handleSetYRange(ActionEvent event) {
+	public void handleSetYRange(ActionEvent event) {																	//Event handler that sets the maximum and minimum value of the y-Axis
 
-		try {
-			yMax = Double.parseDouble(maxYValueTextField.getText());
-			yMin = Double.parseDouble(minYValueTextField.getText());
+		try {																											//Catches all non-valid user inputs such as NaNs and invalid numerals
+			yMax = Double.parseDouble(maxYValueTextField.getText());													//Sets the global variable yMax to the input entered by the user - this variable then becomes the default maximum that will be used when the graph is reset using handleReset
+			yMin = Double.parseDouble(minYValueTextField.getText());													//Sets the global variable yMin to the input entered by the user - this variable then becomes the default minimum that will be used when the graph is reset using handleReset
 
-			yAxis.setUpperBound(yMax);
-			yAxis.setLowerBound(yMin);
+			yAxis.setUpperBound(yMax);																					//Sets the maximum y-Axis value to the value of yMax
+			yAxis.setLowerBound(yMin);																					//Sets the minimum y-Axis value to the value of yMin
 
-			generalStatusLabel.setText("");
+			generalStatusLabel.setText("");																				//Clears the error message displayed if an exception is handled
 
-		} catch (NumberFormatException e) {
-			generalStatusLabel.setText("Enter a valid Y-Axis Value");
-			maxYValueTextField.setText(Double.toString(yMax));
-			minYValueTextField.setText(Double.toString(yMin));
+		} catch (NumberFormatException e) {																				//If a number format exception is handled ->
+			generalStatusLabel.setText("Enter a valid Y-Axis Value");													//An error message is displayed on the Graphing interface
+			maxYValueTextField.setText(Double.toString(yMax));															//The maxYValueTextField is reset to the last valid value held by yMax
+			minYValueTextField.setText(Double.toString(yMin));															//The minYValueTextField is reset to the last valid value held by yMin
 
-			yAxis.setUpperBound(yMax);
-			yAxis.setLowerBound(yMin);
+			yAxis.setUpperBound(yMax);																					//Sets the maximum y-Axis value to the last valid value of yMax
+			yAxis.setLowerBound(yMin);																					//Sets the minimum y-Axis value to the last valid value of yMin
 		}
 
 	}
 
 	@FXML
-	public void addTenNullButtonHandler(ActionEvent event) {
-		XOffsetCounter += 10;
-		for(final DataSeries ds: dataSeries) {
-			ds.addNulls(XOffsetCounter);
+	public void addTenNullButtonHandler(ActionEvent event) {															//Event handler that shifts the data being displayed on the line chart by +10 data samples
+		XOffsetCounter += 10;																							//Incrementer that increments the amount offset that has been applied to the data by +10 and stores it in the XOffsetCounter variable
+		for(final DataSeries axisOfDataSeries: dataSeries) {															
+			axisOfDataSeries.addNulls(XOffsetCounter);
 		}
 		repopulateData();
 		restyleSeries();
@@ -381,16 +365,16 @@ public class GraphController implements Initializable{
 		xAxis.setUpperBound(Double.parseDouble(roundTime.format(dataCollector[numDataSets].getLengthOfTest())));
 		xAxis.setLowerBound(0);
 
-		zoomRect.setManaged(true);
-		zoomRect.setFill(Color.LIGHTBLUE.deriveColor(0, 1, 1, 0.5));
+		userCreatedZoomRectangleBox.setManaged(true);
+		userCreatedZoomRectangleBox.setFill(Color.LIGHTBLUE.deriveColor(0, 1, 1, 0.5));
 		baselineRect.setManaged(true);
 		baselineRect.setFill(Color.LIGHTGOLDENRODYELLOW.deriveColor(0, 1, 1, 0.5));
-		chartContainer.getChildren().remove(zoomRect);
-		chartContainer.getChildren().add(zoomRect);
+		chartContainer.getChildren().remove(userCreatedZoomRectangleBox);
+		chartContainer.getChildren().add(userCreatedZoomRectangleBox);
 		chartContainer.getChildren().remove(baselineRect);
 		chartContainer.getChildren().add(baselineRect);
 
-		setUpZooming(zoomRect, lineChart);
+		setUpZooming(userCreatedZoomRectangleBox, lineChart);
 		
 		if(numDataSets == 0) {
 			for (final DataSeries ds : dataSeries) {
@@ -428,7 +412,7 @@ public class GraphController implements Initializable{
 			}
 		}
 
-		final BooleanBinding disableControls = zoomRect.widthProperty().lessThan(5).or(zoomRect.heightProperty().lessThan(0));
+		final BooleanBinding disableControls = userCreatedZoomRectangleBox.widthProperty().lessThan(5).or(userCreatedZoomRectangleBox.heightProperty().lessThan(0));
 		zoomButton.disableProperty().bind(disableControls);
 
 		if (maxYValueTextField.getText().equals("") && minYValueTextField.getText().equals("")) {
@@ -523,7 +507,7 @@ public class GraphController implements Initializable{
 		dataSourceTitledPaneTwo.setText("");
 		dataDisplayCheckboxesFlowPane.getChildren().clear();
 		dataDisplayCheckboxesFlowPaneTwo.getChildren().clear();
-		dataSeries = FXCollections.observableArrayList(); 
+		dataSeries = FXCollections.observableArrayList();
 		dataSeriesTwo = FXCollections.observableArrayList();
 		numDataSets = 0;
 	}
@@ -567,11 +551,11 @@ public class GraphController implements Initializable{
 		populateData(dataSeries, lineChart);
 		styleSeries(dataSeries, lineChart);
 
-		zoomRect.setManaged(true);
-		zoomRect.setFill(Color.LIGHTBLUE.deriveColor(0, 1, 1, 0.5));
-		chartContainer.getChildren().add(zoomRect);
+		userCreatedZoomRectangleBox.setManaged(true);
+		userCreatedZoomRectangleBox.setFill(Color.LIGHTBLUE.deriveColor(0, 1, 1, 0.5));
+		chartContainer.getChildren().add(userCreatedZoomRectangleBox);
 
-		setUpZooming(zoomRect, lineChart);
+		setUpZooming(userCreatedZoomRectangleBox, lineChart);
 
 		for (final DataSeries ds : dataSeries) {
 			final CheckBox dataToDisplayCheckBox = new CheckBox(ds.getName());
@@ -589,7 +573,7 @@ public class GraphController implements Initializable{
 			});
 		}
 
-		final BooleanBinding disableControls = zoomRect.widthProperty().lessThan(5).or(zoomRect.heightProperty().lessThan(0));
+		final BooleanBinding disableControls = userCreatedZoomRectangleBox.widthProperty().lessThan(5).or(userCreatedZoomRectangleBox.heightProperty().lessThan(0));
 		zoomButton.disableProperty().bind(disableControls);
 
 		if (maxYValueTextField.getText().equals("") && minYValueTextField.getText().equals("")) {
@@ -680,8 +664,8 @@ public class GraphController implements Initializable{
 		final double MagicNumberOne, MagicNumberTwo;
 		MagicNumberOne = 1;
 		MagicNumberTwo = 0.7;
-		Point2D zoomTopLeft = new Point2D(zoomRect.getX(), zoomRect.getY());
-		Point2D zoomBottomRight = new Point2D(zoomRect.getX() + zoomRect.getWidth(), zoomRect.getY() + zoomRect.getHeight());
+		Point2D zoomTopLeft = new Point2D(userCreatedZoomRectangleBox.getX(), userCreatedZoomRectangleBox.getY());
+		Point2D zoomBottomRight = new Point2D(userCreatedZoomRectangleBox.getX() + userCreatedZoomRectangleBox.getWidth(), userCreatedZoomRectangleBox.getY() + userCreatedZoomRectangleBox.getHeight());
 		final NumberAxis yAxis = (NumberAxis) chart.getYAxis();
 		Point2D yAxisInScene = yAxis.localToScene(0, 0);
 		final NumberAxis xAxis = (NumberAxis) chart.getXAxis();
@@ -692,7 +676,7 @@ public class GraphController implements Initializable{
 		double yAxisScale = yAxis.getScale();
 		
 		xRangeLow = (xAxis.getLowerBound() + xOffset / xAxisScale) - MagicNumberOne;
-		xRangeHigh = Double.parseDouble(roundTime.format((xAxis.getLowerBound() + zoomRect.getWidth() / xAxisScale) - MagicNumberTwo) );
+		xRangeHigh = Double.parseDouble(roundTime.format((xAxis.getLowerBound() + userCreatedZoomRectangleBox.getWidth() / xAxisScale) - MagicNumberTwo) );
 	}
 	
 	/*** Sets Up and Performs Zooming ***/
@@ -725,12 +709,12 @@ public class GraphController implements Initializable{
 		});
 	}
 
-	private void doZoom(Rectangle zoomRect, LineChart<Number, Number> chart) {
+	private void doZoom(Rectangle userCreatedZoomRectangleBox, LineChart<Number, Number> chart) {
 		final double MagicNumberOne, MagicNumberTwo;
 		MagicNumberOne = 1.1;
 		MagicNumberTwo = 0.7;
-		Point2D zoomTopLeft = new Point2D(zoomRect.getX(), zoomRect.getY());
-		Point2D zoomBottomRight = new Point2D(zoomRect.getX() + zoomRect.getWidth(), zoomRect.getY() + zoomRect.getHeight());
+		Point2D zoomTopLeft = new Point2D(userCreatedZoomRectangleBox.getX(), userCreatedZoomRectangleBox.getY());
+		Point2D zoomBottomRight = new Point2D(userCreatedZoomRectangleBox.getX() + userCreatedZoomRectangleBox.getWidth(), userCreatedZoomRectangleBox.getY() + userCreatedZoomRectangleBox.getHeight());
 		final NumberAxis yAxis = (NumberAxis) chart.getYAxis();
 		Point2D yAxisInScene = yAxis.localToScene(0, 0);
 		final NumberAxis xAxis = (NumberAxis) chart.getXAxis();
@@ -740,11 +724,11 @@ public class GraphController implements Initializable{
 		double xAxisScale = xAxis.getScale();
 		double yAxisScale = yAxis.getScale();
 		xAxis.setLowerBound((xAxis.getLowerBound() + xOffset / xAxisScale) - MagicNumberOne);
-		xAxis.setUpperBound(Double.parseDouble(roundTime.format(xAxis.getLowerBound() + zoomRect.getWidth() / xAxisScale)));
+		xAxis.setUpperBound(Double.parseDouble(roundTime.format(xAxis.getLowerBound() + userCreatedZoomRectangleBox.getWidth() / xAxisScale)));
 		yAxis.setLowerBound((yAxis.getLowerBound() + yOffset / yAxisScale) - MagicNumberTwo);
-		yAxis.setUpperBound(yAxis.getLowerBound() - zoomRect.getHeight() / yAxisScale);
-		zoomRect.setWidth(0);
-		zoomRect.setHeight(0);
+		yAxis.setUpperBound(yAxis.getLowerBound() - userCreatedZoomRectangleBox.getHeight() / yAxisScale);
+		userCreatedZoomRectangleBox.setWidth(0);
+		userCreatedZoomRectangleBox.setHeight(0);
 
 
 		for (final DataSeries ds : dataSeries) {
