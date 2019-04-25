47bc890b4a0c83f7abb1150038b01cd7f2c5e272
Vineeth Voruganti <ryanmmlarue@gmail.com>
2019-04-25 18:14:18 -0400

    Update PendulumSpreadsheetController.java

--- src/dataorganizer/PendulumSpreadsheetController.java
+++ src/dataorganizer/PendulumSpreadsheetController.java
@@ -4,44 +4,88 @@ import com.aspose.cells.*;
 
 import java.util.List;
 
+import javax.swing.JOptionPane;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JFileChooser;
+
 public class PendulumSpreadsheetController {
 
 
     //TODO: Test if Workbook can be saved to a different location than its origin (Ex. Leave template in src and save the updated version to Documents)
     //TODO: Dynamically get path of template
     private Workbook workbook;
-    private String workbookPath = "C:\\Users\\Mason\\Documents\\Lab Templates\\Pendulum Template REV-Q3.xlsx";
+   //private String workbookPath = "C:\\Users\\Conference\\Documents\\Lab Templates\\Pendulum Template REV-Q3.xlsx";
+    private String workbookPath = "C:\\Users\\Kinobo\\Desktop\\JavaDashboardMaster\\Pendulum Template REV-Q3.xlsx";
+    
+    private String documentsPath;
+    
+    public PendulumSpreadsheetController() {
+        documentsPath = System.getProperty("user.home") + "/Documents" + "/Lab Templates" +"Pendulum Template REV-Q3.xlsx";
 
-
-
-    public void loadPendulumParameters (double pendulumLength, double pendulumMass, double moduleMass, double moduleDistanceFromAOR){ //
-        try{
-            workbook = new Workbook(workbookPath);
+        try {
+            this.workbook = new Workbook(workbookPath);
+        }catch(Exception e){
+            System.out.println("Invalid Workbook Path");
         }
-        catch(Exception eg){
-            System.out.println("Failed to open workbook");
+    }
+    
+//    public void chooseoutputPath() {
+////    	JFrame outputpathChooser = new JFrame();
+////    	String test = JOptionPane.showInputDialog(outputpathChooser, "Enter or choose the save location for spreadsheet");
+////    	
+//    	JFrame output = new JFrame();
+//    	
+//    	JFileChooser outputpathChooser;
+//    	outputpathChooser = new JFileChooser();
+//    	
+//    	output.add(outputpathChooser);
+//    	output.setVisible(true);
+//    	
+//    	System.out.println(test);
+//    }
+    
+    public void chooseoutputPath(JLabel label ) {
+		JFileChooser chooser;
+		chooser = new JFileChooser();
+		chooser.setCurrentDirectory(new java.io.File("."));
+		chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+		chooser.setAcceptAllFileFilterUsed(false);
+		if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+			saveWorkbook(chooser.getSelectedFile().toString());
+		}
+		else {
+			label.setText("Invalid File Path");
+		}
+	}
+    	
+    public void saveWorkbook(String outputPath){
+        try {
+            workbook.save(outputPath, FileFormatType.XLSX);
+
+        }catch(Exception e){
+            System.out.println("Invalid output path");
         }
+
+    }
+
+
+    public void loadPendulumParameters (double pendulumLength, double pendulumMass, double moduleMass, double moduleDistanceFromAOR){
         workbook.getWorksheets().get(4).getCells().get("C7").setValue(pendulumLength);
         workbook.getWorksheets().get(4).getCells().get("C8").setValue(pendulumMass);
         workbook.getWorksheets().get(4).getCells().get("C9").setValue(moduleMass);
         workbook.getWorksheets().get(4).getCells().get("C10").setValue(moduleDistanceFromAOR);
 
+//        System.out.println(pendulumLength);
+//        System.out.println(pendulumMass);
+//        System.out.println(moduleMass);
+//        System.out.println(moduleDistanceFromAOR);
 
-        System.out.println(pendulumLength);
-        System.out.println(pendulumMass);
-        System.out.println(moduleMass);
-        System.out.println(moduleDistanceFromAOR);
-
-        /*try{
-            workbook.save(workbookPath, FileFormatType.XLSX);
-        }
-        catch(Exception e){
-            System.out.println("Invalid Path");
-        }*/
     }
 
 
-    //TODO: UNTESTED
+
     /**
      * Fills the Pendulum Template with all of the data recorded during a module test
      * @param rowOffset The offset integer used to identify the first row for data to be added
@@ -57,14 +101,6 @@ public class PendulumSpreadsheetController {
                     workbook.getWorksheets().get(0).getCells().get(columnIndex + rowOffset, axis-1).setValue(ColumnData.get(columnIndex).intValue());  //Adds the data held at columnIndex to each row of the data
                 }
             }
-        }
-
-        try{
-            workbook.save(workbookPath, FileFormatType.XLSX);
-        }
-        catch(Exception e){
-        	System.out.println(e.getStackTrace());
-            System.out.println("Invalid Path");
         }
 
     }
