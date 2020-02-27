290c8e1d748380f2824c4527f3b305f7e9d944ba
abqw112 <abqw112@gmail.com>
2020-02-26 19:02:27 -0500

    Finished work on testing graph

--- src/dataorganizer/GraphNoSINCController.java
+++ src/dataorganizer/GraphNoSINCController.java
@@ -3,19 +3,66 @@ package dataorganizer;
 import static java.lang.Thread.getAllStackTraces;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart;
+import javafx.scene.chart.XYChart.Series;
 
 public class GraphNoSINCController implements Initializable {
 
+	
+	private static GenericTest genericTestOne; //object for holding the generic test for module 1
+	private static GenericTest genericTestTwo; //object for holding the generic test for module 2
+	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		// TODO Auto-generated method stub
 		
 	}
+	
+	public void assignGenericTestOne(GenericTest genericTestOne) { //assigns generic test object to module 1
+		this.genericTestOne = genericTestOne;
+	}
+	
+	public void assignGenericTestTwo(GenericTest genericTestTwo) { //assigns generic test object to module 2
+		this.genericTestTwo = genericTestTwo;
+	}
+	
+	public void createSeries(ArrayList<Double> timeData, ArrayList<Double> samplesData) {
+		
+		ArrayList<Double> cleanTimeData = new ArrayList<Double>();										// setup new array list for time data
+		ArrayList<Double> cleanSamplesData = new ArrayList<Double>();									// setup new array list for samples data
+		
+		int resolution = 1920;																			//the interval between data points to take for graph  -- GREATLY SPEEDS UP DISPLAY TIME, NO DATA IS LOST
+		
+		for(int i = 0; i < timeData.size(); i+= resolution) {											//takes every "resolution" sample from the input data and adds it to the clean array lists for displaying
+			cleanTimeData.add(timeData.get(i));
+			cleanSamplesData.add(samplesData.get(i));
+		}
+		
+		XYChart.Series<Number, Number> series = new XYChart.Series<Number, Number>();					//setups a new series
+		ObservableList<XYChart.Data<Number, Number>> seriesData = FXCollections.observableArrayList();
+		
+		for (int i = 0; i < cleanTimeData.size(); i++) {
+			
+            seriesData.add(new XYChart.Data<>(cleanTimeData.get(i), cleanSamplesData.get(i)));
+            
+        }
+		
+		lineChart.getData().add(series);
+		series.setData(seriesData);
+		System.out.println(series.getName());
+	}
+	@FXML
+	private LineChart<Number,Number> lineChart;
 	
 	@FXML
 	public void handleReset(ActionEvent event) {
--- src/dataorganizer/GraphNoSINCMain.java
+++ src/dataorganizer/GraphNoSINCMain.java
@@ -1,5 +1,8 @@
 package dataorganizer;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -10,22 +13,43 @@ import javafx.stage.Stage;
 public class GraphNoSINCMain extends Application {
 
     //REV 36
+	private static GraphNoSINCController controller;
+	private static ArrayList<Double> testDataSamples;
+	private static ArrayList<Double> testDataTime;
     @Override
     public void start(Stage primaryStage) throws Exception{
     	System.out.println("test");
-        Parent root = FXMLLoader.load(getClass().getResource("GraphNoSINC.fxml"));
-       
+    	FXMLLoader loader = new FXMLLoader((getClass().getResource("GraphNoSINC.fxml")));
+        Parent root = loader.load();
+        controller = loader.getController();
         
         primaryStage.setTitle("BioForce Experiment Graph");
         Scene scene = new Scene(root, 690, 500);
         primaryStage.setScene(scene);
         //scene.getStylesheets().add(getClass().getResource("EducatorModeCSS.css").toExternalForm());
         primaryStage.show();
+        
         primaryStage.setResizable(false);
+
+        
+        controller.createSeries(testDataTime, testDataSamples);
     }
 
     public static void main(String[] args) {
-    	 
-        launch(args);
+    	//ArrayList<Double> testDataSamples = new ArrayList<Double>();
+    	//ArrayList<Double>[] testDataTime = new double[9600];
+    	int size = 96000;
+    	testDataSamples = new ArrayList<Double>();
+    	testDataTime = new ArrayList<Double>();
+    	System.out.println(testDataSamples.size());
+    	for(int i = 0; i < size; i++) {
+    		testDataSamples.add( 5 * Math.sin(i / 1000.0) * (((i-10000)/1000.0) / (1 + (((i-10000)/1000.0)*((i-10000)/1000.0)))));
+    		testDataTime.add(i / 960.0);
+    	}
+    	launch(args);
+    	
+    	
+    	
+       
     }
 }
