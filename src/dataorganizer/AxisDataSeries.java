9a51cd1e4502e143434d1e2f836ce8158e021b41
William Liao <abqw112@gmail.com>
2020-03-10 15:28:43 -0400

    add testing-ready versions of AxisDataSeries and GenericTest

--- src/dataorganizer/AxisDataSeries.java
+++ src/dataorganizer/AxisDataSeries.java
@@ -1,5 +1,6 @@
 package dataorganizer;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Dictionary;
@@ -29,6 +30,9 @@ public class AxisDataSeries {
 	// flags whether graph should be drawn
 	private boolean isActive;
 	
+	//samples per second in passed in data series
+	private int sampleRate;
+	
 	/**
 	 * AxisDataSeries constructor for data NOT natively recorded by the module OR if it is from the magnetometer.
 	 * @param time
@@ -36,11 +40,12 @@ public class AxisDataSeries {
 	 * @param axis
 	 * @param signData
 	 */
-	public AxisDataSeries(List<Double> time, List<Double> data, AxisType axis, boolean signData) {
+	public AxisDataSeries(List<Double> time, List<Double> data, AxisType axis, boolean signData, int sampleRate) {
 		
 		this.time = (Double[]) time.toArray();
 		this.originalData = (Double[]) data.toArray();
 		this.axis = axis;
+		this.sampleRate = sampleRate;
 		
 		if (signData) {
 			
@@ -67,11 +72,12 @@ public class AxisDataSeries {
 	 * @param accelOffsets
 	 * @param accelSensitivity
 	 */
-	public AxisDataSeries(List<Double> time, List<Double> data, AxisType axis, int[] accelOffsets, int accelSensitivity) {
+	public AxisDataSeries(List<Double> time, List<Double> data, AxisType axis, int[] accelOffsets, int accelSensitivity, int sampleRate) {
 		
 		this.time = (Double[]) time.toArray();
 		this.originalData = (Double[]) data.toArray();
 		this.axis = axis;
+		this.sampleRate = sampleRate;
 		
 		for (int i = 0; i < this.originalData.length; i++) {
 			
@@ -102,11 +108,12 @@ public class AxisDataSeries {
 	 * @param axis
 	 * @param gyroSensitivity
 	 */
-	public AxisDataSeries(List<Double> time, List<Double> data, AxisType axis, int gyroSensitivity) {
+	public AxisDataSeries(List<Double> time, List<Double> data, AxisType axis, int gyroSensitivity, int sampleRate) {
 		
 		this.time = (Double[]) time.toArray();
 		this.originalData = (Double[]) data.toArray();
 		this.axis = axis;
+		this.sampleRate = sampleRate;
 		
 		for (int i = 0; i < this.originalData.length; i++) {
 			
@@ -128,6 +135,11 @@ public class AxisDataSeries {
 	// returns smoothed data using a middle-based rolling average
 	public ObservableList<XYChart.Series<Number, Number>> getMovingAvg(int sampleBlockSize) {
 		
+		if(sampleBlockSize == 0) {
+			smoothedData = originalData;
+			return createSeries();
+		}
+		
 		for (int i = sampleBlockSize/2; i < smoothedData.length - sampleBlockSize/2; i++) {
 			double localTotal = 0;
 			for (int j = (i - sampleBlockSize/2); j<(i+sampleBlockSize/2); j++) {
@@ -142,10 +154,10 @@ public class AxisDataSeries {
 	// doing trapezoidal rule for integration
 	public List<Double> integrate() {
 		
-		Double[] result = new Double[smoothedData.length];
+		Double[] result = new Double[originalData.length];
 		
-		for(int i = 1; i < smoothedData.length; i++) {
-			result[i] = result[i-1] + (smoothedData[i] + smoothedData[i-1])/2 * (time[i] - time[i-1]);
+		for(int i = 1; i < originalData.length; i++) {
+			result[i] = result[i-1] + (originalData[i] + originalData[i-1])/2 * (time[i] - time[i-1]);
 		}
 		
 		// calculate the first integrated sample
@@ -155,17 +167,17 @@ public class AxisDataSeries {
 		return Arrays.asList(result);
 	}
 	
+	//get slope of secant line between point directly before and after
 	public List<Double> differentiate() {
 		
-		Double[] result = new Double[smoothedData.length];
-		
-		for(int i = 1; i < smoothedData.length - 1; i++) {
-			result[i] = (smoothedData[i+1]-smoothedData[i-1])/2;
+		Double[] result = new Double[originalData.length]; 
+		for(int i = 1; i < originalData.length - 1; i++) {
+			result[i] = (originalData[i+1]-originalData[i-1])/(time[i+1]-time[i-1]);
 		}
 		
 		//fill in first and last data point
 		result[0] = result[1];
-		result[smoothedData.length-1] = result[smoothedData.length-2];
+		result[originalData.length-1] = result[originalData.length-2];
 
 		return Arrays.asList(result);
 	}
@@ -185,12 +197,74 @@ public class AxisDataSeries {
         return FXCollections.observableArrayList(Collections.singleton(series));
     }
 	
+	public Double getSlope(Double time) {
+		//uses secant line method b/w sample directly before and after
+		int i = (int) Math.round(time*this.sampleRate);
+		Double slope = (originalData[i+1]-originalData[i-1])/(this.time[i+1]-this.time[i-1]);
+		return slope;
+	}
+	
+	
+	public Double getSlope(Double startTime, Double endTime) {
+		Double slope = 0.0;
+		
+		if (startTime == endTime) {
+			int i = (int) Math.round(startTime*this.sampleRate);
+			slope = (originalData[i+1]-originalData[i-1])/(this.time[i+1]-this.time[i-1]);
+			return slope;
+		}
+		else {
+		int i = (int) Math.round(startTime*this.sampleRate);
+		int j = (int) Math.round(endTime*this.sampleRate);
+		slope = (originalData[j]-originalData[i])/(this.time[j]-this.time[i]);}
+		return slope;
+	}
+	
+	public Double getAreaUnder(Double startTime, Double endTime) {
+		//trapezoid method
+		Double area = 0.0;
+		int i = (int) Math.round(startTime*this.sampleRate);
+		int j = (int) Math.round(endTime*this.sampleRate);
+		area = ((originalData[i]+originalData[j])/2)*(this.time[j]-this.time[i]);
+		return area;
+	}
+
+	public Double[] getSmoothedData() {
+		return this.smoothedData;
+	}
+	
+	public void setSmoothedData(Double[] data) {
+		this.smoothedData = data;
+	}
+	
+	public void setSmoothedDataPoint(int index, Double value) {
+		this.smoothedData[index] = value;	
+	}
+	
 	public Double[] getOriginalData() { 
 		return this.originalData;
 	}
 	
 	public void setOriginalData(Double[] data) {
 		this.originalData = data;
+	}
+	
+	public void setOriginalDataPoint(int index, Double value ) {
+		this.originalData[index] = value;	
+	}
+	
+	/**
+	 * Used by Graph GUI to get time data from AxisDataSeries instances.
+	 */
+	public ArrayList<Double> getTime() {
+		return (ArrayList<Double>) Arrays.asList(time);
+	}
+	
+	/**
+	 * Used by Graph GUI to get samples from AxisDataSeries instances.
+	 */
+	public ArrayList<Double> getSamples() {
+		return (ArrayList<Double>) Arrays.asList(smoothedData);
 	}
 	
 	public boolean getActive() {
@@ -200,31 +274,5 @@ public class AxisDataSeries {
 	public void setActive(boolean b) {
 		this.isActive = b;
 	}
-	
-	public Double getSlope(Double time) {
-		//TODO
-		Double slope = 0d;
-		return slope;
-	}
-	
-	public Double getSlope(Double startTime, Double endTime) {
-		//TODO
-		Double slope = 0d;
-		return slope;
-	}
-	
-	public Double getAreaUnder(Double startTime, Double endTime) {
-		//TODO
-		Double area = 0d;
-		return area;
-	}
-
-	public Double [] getSmoothedData() {
-		return this.smoothedData;
-	}
-	
-	public void setSmoothedData(int index, double value ) {
-		this.smoothedData[index] = value;
-		
-	}
 }
+	
