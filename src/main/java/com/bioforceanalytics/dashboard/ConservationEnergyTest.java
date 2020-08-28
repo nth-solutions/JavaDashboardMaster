521f6a7015f18797f35c8d41f416e5bee1910abf
shrey150 <shrey150@yahoo.com>
2020-08-28 03:41:52 -0400

    refactor two-module tests and CSV saving process

--- src/main/java/com/bioforceanalytics/dashboard/ConservationEnergyTest.java
+++ src/main/java/com/bioforceanalytics/dashboard/ConservationEnergyTest.java
@@ -1,42 +1,12 @@
 package com.bioforceanalytics.dashboard;
 
-import java.util.ArrayList;
-
-public class ConservationEnergyTest{
+public class ConservationEnergyTest extends TwoModuleTest {
 
 	private double mass;
 	private double momentOfInertia;
 	private double radiusOfArm;
 	private double distanceDropped;
-	private GenericTest moduleOne;
-	private GenericTest moduleTwo;
 
-	public boolean isFilled(){
-		return (moduleOne != null && moduleTwo != null);
-	}
-
-	public GenericTest getModuleOne(){
-		return moduleOne;
-	}
-	public GenericTest getModuleTwo(){
-		return moduleTwo;
-	}
-
-	public void addModule(ArrayList<Integer> testParameters, int[] finalData, int[][] MPUMinMax) {
-
-		if (moduleOne == null) {
-			moduleOne = new ConservationEnergyModule(testParameters,finalData, MPUMinMax,(mass),this);
-			moduleOne.setGraphTitle("Conservation of Energy");
-			moduleOne.setDefaultAxes(AxisType.DispY);
-		} else if (moduleTwo == null) {
-			moduleTwo = new ConservationEnergyModule(testParameters, finalData, MPUMinMax, (radiusOfArm * momentOfInertia), this);
-			moduleTwo.setGraphTitle("Conservation of Energy");
-			moduleTwo.setDefaultAxes(AxisType.AngVelZ);
-		} else {
-			System.out.println("Conservation of Energy Error");
-		}
-
-	}
 	public ConservationEnergyTest(double mass, double momentOfInertia, double radiusOfArm, double distanceDropped) {
 
 		this.mass = mass;
@@ -46,18 +16,43 @@ public class ConservationEnergyTest{
 
 	}
 
-	public void setupExperimentPanel(ExperimentPanel panel){
+	@Override
+	public int addModule(GenericTest test) {
+
+		int moduleNumber = super.addModule(test);
+
+		test.setGraphTitle("Conservation of Energy");
+
+		if (moduleNumber == 1) {
+			test.setDefaultAxes(AxisType.DispY);
+		}
+		else if (moduleNumber == 2) {
+			test.setDefaultAxes(AxisType.AngVelZ);
+		}
+
+		return moduleNumber;
+
+	}
+
+	public void setupExperimentPanel(ExperimentPanel panel) {
+
 		panel.setExperimentName("Conservation of Energy");
+
 		panel.addParamName("Mass");
-		panel.addParamValue(mass +" kg");
+		panel.addParamValue(mass + " kg");
+
 		panel.addParamName("Moment of Inertia");
-		panel.addParamValue(momentOfInertia +" kg-m²");
+		panel.addParamValue(momentOfInertia + " kg-m²");
+
 		panel.addParamName("Radius of Torque Arm");
-		panel.addParamValue(radiusOfArm +" m");
+		panel.addParamValue(radiusOfArm + " m");
+
 		panel.addParamName("Distance Dropped");
-		panel.addParamValue(distanceDropped +" m");
+		panel.addParamValue(distanceDropped + " m");
+
 		panel.applyParams();
 		panel.setFormulaResult("Total Energy: " + mass * distanceDropped * 9.8 + " J");
+
 	}
 
 
