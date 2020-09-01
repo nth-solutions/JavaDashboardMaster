3fbb59f595a367632a6c7624b2094f35333f9d08
shrey150 <shrey150@yahoo.com>
2020-09-01 19:24:58 -0400

    fix color menu bug

--- src/main/java/com/bioforceanalytics/dashboard/EducatorModeControllerFX.java
+++ src/main/java/com/bioforceanalytics/dashboard/EducatorModeControllerFX.java
@@ -2082,40 +2082,4 @@ public class EducatorModeControllerFX implements Initializable {
         return t;
     }
 
-    /**
-     * Gets a 3 letter abbreviation for the passed in month for the automatic test title generation
-     *
-     * @param month an integer 0-11 that corresponds to the month with 0 = January and 11 = December
-     * @return The 3 letter abbreviation for the month
-     */
-    public String getMonth(int month) {
-        switch (month) {
-            case (0):
-                return "JAN";
-            case (1):
-                return "FEB";
-            case (2):
-                return "MAR";
-            case (3):
-                return "APR";
-            case (4):
-                return "MAY";
-            case (5):
-                return "JUN";
-            case (6):
-                return "JUL";
-            case (7):
-                return "AUG";
-            case (8):
-                return "SEP";
-            case (9):
-                return "OCT";
-            case (10):
-                return "NOV";
-            case (11):
-                return "DEC";
-        }
-        return "NOP";
-    }
-
 }
